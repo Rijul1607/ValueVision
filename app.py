192ad056f76b6310060457cee9f77c7d6fd72818
rijul <rijulgulati16@gmail.com>
2025-02-08 21:29:55 +0530

    changes

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ if __name__ == "__main__":
     util.load_saved_artifacts()
 
     # Fix for Render: Bind to 0.0.0.0 and use the correct port
-    port = int(os.environ.get("PORT", 10000))  # Use Render's PORT
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT
     app.run(host="0.0.0.0", port=port, debug=True)
